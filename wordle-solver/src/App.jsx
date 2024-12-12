085ec1afbd58429e020c0e310d688cba3d28b3d5
Oliver Persson <oliver.persson.8578@student.lu.se>
2024-12-12 19:06:37 +0100

    Added placeholder keyboard

--- wordle-solver/src/App.jsx
+++ wordle-solver/src/App.jsx
@@ -1,28 +1,124 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function initGrid() {
+const keyboard = ['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM']
+
+function MainGrid() {
   return (
-    [...Array(6)].map((_, i) => (
-      <div className="row" key={`row${i}`}>
-        {[...Array(5)].map((_, j) => (
-          <input className="col" type="text" maxLength="1" key={`col${i}${j}`}/>
-        ))}
+    <div className="main-grid">
+        {[...Array(6)].map((_, i) => (
+        <div className="row grid-row" key={`row${i}`}>
+          {[...Array(5)].map((_, j) => (
+            <input 
+            className="col grid-col"  
+              type="text" 
+              maxLength="1" 
+              key={`col${i}${j}`}
+              id={`col${i}${j}`}
+              onKeyDown={inputCharacter}>
+            </input>
+          ))}
+        </div>
+      ))}
       </div>
-    ))
   );
 }
 
+function KeyBoard() {
+  return (
+    <div>
+      <div className='horizontal-list'>
+      {keyboard[0].split('').map((_, i) => (
+          <li>
+            <label
+              className='key-label'
+              key={`key_${keyboard[0][i]}`} 
+              id={`key_${keyboard[0][i]}`} 
+              onClick={inputCharacter}>
+              {keyboard[0][i]}
+            </label>
+          </li>
+        ))}
+      </div>
+
+      <div className='horizontal-list'>
+        {keyboard[1].split('').map((_, i) => (
+          <li>
+            <label 
+              className='key-label'
+              key={`key_${keyboard[1][i]}`} 
+              id={`key_${keyboard[1][i]}`} 
+              onClick={inputCharacter}>
+              {keyboard[1][i]}
+            </label>
+          </li>
+        ))}
+      </div>
+
+      <div className='horizontal-list'>
+          <label 
+            className='enter-key-label'
+            key={'key_ENTER'} 
+            id={'key_ENTER'} 
+            onClick={inputCharacter}>
+            {'ENTER'}
+          </label>
+        {keyboard[2].split('').map((_, i) => (
+          <li>
+            <label 
+              className='key-label'
+              key={`key_${keyboard[2][i]}`} 
+              id={`key_${keyboard[2][i]}`} 
+              onClick={inputCharacter}>
+              {keyboard[2][i]}
+            </label>
+          </li>
+        ))}
+        <label 
+          className='del-key-label'
+          key={'key_DEL'} 
+          id={'key_DEL'} 
+          onClick={inputCharacter}>
+          {'DEL'}
+        </label>
+      </div>
+    </div>
+  );
+}
+
+function inputCharacter(event) {
+  event.preventDefault()
+  console.log(event.target.id)
+  /* Implement later
+  let char = event.key
+  event.target.value = char.toUpperCase()
+  event.target.blur()
+
+  const next = event.target.nextElementSibling
+  if (next && next.id[4] != '0') {
+    next.focus()
+  }
+  */
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [row, setRow] = useState(0);
+  const [col, setCol] = useState(0);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const firstInput = document.getElementById('col00');
+    if (firstInput) {
+      firstInput.focus();
+    }
+  }, []);
 
   return (
     <>
-      <div className="main-grid">
-        {initGrid()}
-      </div>
+      <MainGrid />
+      <KeyBoard />
 
       <div>
         <a href="https://vite.dev" target="_blank">
