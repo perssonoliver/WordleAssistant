632316324c9c885a7d9f4ebccc873939e6a9d07f
Oliver Persson <oliver.persson.8578@student.lu.se>
2024-12-13 18:12:16 +0100

    Added simple handler for clicking keys

--- wordle-solver/src/App.jsx
+++ wordle-solver/src/App.jsx
@@ -16,8 +16,7 @@ function MainGrid() {
               type="text" 
               maxLength="1" 
               key={`col${i}${j}`}
-              id={`col${i}${j}`}
-              onKeyDown={inputCharacter}>
+              id={`col${i}${j}`}>
             </input>
           ))}
         </div>
@@ -26,87 +25,108 @@ function MainGrid() {
   );
 }
 
-function KeyBoard() {
-  return (
-    <div>
-      <div className='horizontal-list'>
-      {keyboard[0].split('').map((_, i) => (
-          <li>
-            <label
-              className='key-label'
-              key={`key_${keyboard[0][i]}`} 
-              id={`key_${keyboard[0][i]}`} 
-              onClick={inputCharacter}>
-              {keyboard[0][i]}
-            </label>
-          </li>
-        ))}
-      </div>
-
-      <div className='horizontal-list'>
-        {keyboard[1].split('').map((_, i) => (
-          <li>
-            <label 
-              className='key-label'
-              key={`key_${keyboard[1][i]}`} 
-              id={`key_${keyboard[1][i]}`} 
-              onClick={inputCharacter}>
-              {keyboard[1][i]}
-            </label>
-          </li>
-        ))}
-      </div>
-
-      <div className='horizontal-list'>
-          <label 
-            className='enter-key-label'
-            key={'key_ENTER'} 
-            id={'key_ENTER'} 
-            onClick={inputCharacter}>
-            {'ENTER'}
-          </label>
-        {keyboard[2].split('').map((_, i) => (
-          <li>
-            <label 
-              className='key-label'
-              key={`key_${keyboard[2][i]}`} 
-              id={`key_${keyboard[2][i]}`} 
-              onClick={inputCharacter}>
-              {keyboard[2][i]}
-            </label>
-          </li>
-        ))}
-        <label 
-          className='del-key-label'
-          key={'key_DEL'} 
-          id={'key_DEL'} 
-          onClick={inputCharacter}>
-          {'DEL'}
-        </label>
-      </div>
-    </div>
-  );
-}
-
-function inputCharacter(event) {
-  event.preventDefault()
-  console.log(event.target.id)
-  /* Implement later
-  let char = event.key
-  event.target.value = char.toUpperCase()
-  event.target.blur()
-
-  const next = event.target.nextElementSibling
-  if (next && next.id[4] != '0') {
-    next.focus()
-  }
-  */
-}
-
 function App() {
   const [row, setRow] = useState(0);
   const [col, setCol] = useState(0);
   const [count, setCount] = useState(0);
+
+  function KeyBoard() {
+    return (
+      <div>
+        <div className='horizontal-list'>
+        {keyboard[0].split('').map((_, i) => (
+            <li>
+              <label
+                className='key-label'
+                key={`key_${keyboard[0][i]}`} 
+                id={`key_${keyboard[0][i]}`} 
+                onClick={inputCharacter}>
+                {keyboard[0][i]}
+              </label>
+            </li>
+          ))}
+        </div>
+  
+        <div className='horizontal-list'>
+          {keyboard[1].split('').map((_, i) => (
+            <li>
+              <label 
+                className='key-label'
+                key={`key_${keyboard[1][i]}`} 
+                id={`key_${keyboard[1][i]}`} 
+                onClick={inputCharacter}>
+                {keyboard[1][i]}
+              </label>
+            </li>
+          ))}
+        </div>
+  
+        <div className='horizontal-list'>
+            <label 
+              className='enter-key-label'
+              key={'key_ENTER'} 
+              id={'key_ENTER'} 
+              onClick={inputCharacter}>
+              {'ENTER'}
+            </label>
+          {keyboard[2].split('').map((_, i) => (
+            <li>
+              <label 
+                className='key-label'
+                key={`key_${keyboard[2][i]}`} 
+                id={`key_${keyboard[2][i]}`} 
+                onClick={inputCharacter}>
+                {keyboard[2][i]}
+              </label>
+            </li>
+          ))}
+          <label 
+            className='del-key-label'
+            key={'key_DEL'} 
+            id={'key_DEL'} 
+            onClick={inputCharacter}>
+            {'DEL'}
+          </label>
+        </div>
+      </div>
+    );
+  }
+  
+  function inputCharacter(event) {
+    event.preventDefault()
+    console.log(event.target.id)
+  
+    if (event.target.id === 'key_ENTER') {
+      // todo
+      return
+    } 
+  
+    if (event.target.id === 'key_DEL') {
+      // todo
+      return
+    }
+  
+    const char = event.target.id[4]
+    if (col == 5) {
+      return
+    }
+    const input = document.getElementById(`col${row}${col}`)
+    input.value = char
+    input.blur()
+    setCol(col + 1)
+  
+  
+    /* Implement later
+    let char = event.key
+    event.target.value = char.toUpperCase()
+    event.target.blur()
+  
+    const next = event.target.nextElementSibling
+    if (next && next.id[4] != '0') {
+      next.focus()
+    }
+    */
+  }
 
   useEffect(() => {
     const firstInput = document.getElementById('col00');
