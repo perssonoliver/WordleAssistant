25e0eee3189cbe4cbf8ed090e0ad37aa167ba7bf
Oliver Persson <oliver.persson.8578@student.lu.se>
2024-12-14 19:38:25 +0100

    Color refactoring

--- wordle-solver/src/MainGrid.jsx
+++ wordle-solver/src/MainGrid.jsx
@@ -1,34 +1,30 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-const grey = 'rgb(59, 59, 59)';
-const yellow = 'rgb(204, 175, 13)';
-const green = 'rgb(34, 150, 23)';
+const colors = ['rgb(59, 59, 59)', 'rgb(204, 175, 13)', 'rgb(34, 150, 23)']
 
 function MainGrid({ changeColor, getColor }) {
 
   function changeColorHandler(event) {
     if (event.target.value === '') {
-      return;
+      return
     }
 
-    const row = event.target.id[3];
-    const col = event.target.id[4];
-    const color = getColor(row, col);
+    const row = event.target.id[3]
+    const col = event.target.id[4]
+    const color = getColor(row, col)
 
     let newColor;
-    if (color === '' || color === green) {
-      newColor = grey;
-    } else if (color === grey) {
-      newColor = yellow;
-    } else if (color === yellow) {
-      newColor = green;
+    if (color === '') {
+      newColor = colors[0]
+    } else {
+      newColor = colors[(colors.indexOf(color) + 1) % colors.length]
     }
 
-    changeColor(row, col, newColor);
+    changeColor(row, col, newColor)
 
-    event.target.style.backgroundColor = newColor;
-    event.target.style.borderColor = newColor;
+    event.target.style.backgroundColor = newColor
+    event.target.style.borderColor = newColor
   }
 
   return (
