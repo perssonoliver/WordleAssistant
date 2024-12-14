5e217d4ff27651e40a72ec521398650e9a6caf26
Oliver Persson <oliver.persson.8578@student.lu.se>
2024-12-14 19:53:14 +0100

    Refactor colors again

--- wordle-solver/src/App.jsx
+++ wordle-solver/src/App.jsx
@@ -1,8 +1,25 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import dict from './dict.mjs'
 import Keyboard from './Keyboard'
 import MainGrid from './MainGrid'
 import WordList from './WordList'
+
+const COLORS = ['rgb(59, 59, 59)', 'rgb(204, 175, 13)', 'rgb(34, 150, 23)']
+
+const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let validLetters = initLetters()
+
+function initLetters() {
+  let letters = {}
+  for (let i = 0; i < 6; i++) {
+    letters[i] = {}
+    for (let j = 0; j < 5; j++) {
+      letters[i][j] = ALPHABET
+    }
+  }
+  return letters
+}
 
 function defaultColors() {
   const colors = {};
@@ -16,54 +33,26 @@ function defaultColors() {
 }
 
 function App() {
-  const [row, setRow]       = useState(0);
-  const [col, setCol]       = useState(0);
-  const [colors, setColors] = useState(defaultColors);
+  const [row, setRow]           = useState(0);
+  const [col, setCol]           = useState(0);
+  const [colors, setColors]     = useState(defaultColors);
+  const [wordList, setWordList] = useState([])
 
-  function getColor(row, col) {
-    return colors[row][col];
-  }
+  function changeColor(target, row, col) {
+    const color = colors[row][col]
 
-  function changeColor(row, col, newColor) {
+    let newColor;
+    if (color === '') {
+      newColor = COLORS[0]
+    } else {
+      newColor = COLORS[(COLORS.indexOf(color) + 1) % COLORS.length]
+    }
+
     const newColors = { ...colors, [row]: { ...colors[row], [col]: newColor } };
     setColors(newColors);
-  }
-  
-  function setBoxCharacter(event) {
-    event.preventDefault()
-    console.log(event.target.id)
-  
-    if (event.target.id === 'key_ENTER') {
-      if (row == 5 || col != 5 || !rowColored()) {
-        return
-      }
 
-      updateWordList()
-      setRow(row + 1)
-      setCol(0)
-      return
-    } 
-  
-    if (event.target.id === 'key_DEL') {
-      if (col == 0) {
-        return
-      }
-
-      const box = document.getElementById(`col${row}${col - 1}`)
-      box.value = ''
-      box.blur()
-      setCol(col - 1)
-      return
-    }
-  
-    const char = event.target.id[4]
-    if (col == 5) {
-      return
-    }
-    const box = document.getElementById(`col${row}${col}`)
-    box.value = char
-    box.blur()
-    setCol(col + 1)
+    target.style.backgroundColor = newColor
+    target.style.borderColor = newColor
   }
 
   function rowColored() {
@@ -74,6 +63,64 @@ function App() {
       }
     }
     return true
+  }
+  
+  function setBoxCharacter(event) {
+    event.preventDefault()
+    console.log(event.target.id)
+  
+    if (event.target.id === 'key_ENTER') {
+      if (row == 4 || col != 5 || !rowColored()) {
+        return
+      }
+      updateWordList()
+      setRow(row + 1)
+      setCol(0)
+      return
+    } 
+  
+    if (event.target.id === 'key_DEL') {
+      if (col == 0) {
+        return
+      }
+      const box = document.getElementById(`col${row}${col - 1}`)
+      box.value = ''
+      box.blur()
+      setCol(col - 1)
+      return
+    }
+  
+    if (col == 5) {
+      return
+    }
+
+    const char = event.target.id[4]
+    const box = document.getElementById(`col${row}${col}`)
+    box.value = char
+    box.blur()
+    setCol(col + 1)
+  }
+
+  function updateWordList() {
+    const rowLetters = [...Array(5)]
+    for (let i = 0; i < 5; i++) {
+      rowLetters[i] = document.getElementById(`col${row}${i}`).value
+    }
+
+    for (let i = 0; i < 5; i++) {
+      if (rowLetters[i] === '') {
+        return
+      }
+    }
+
+
+
+
+    let newWords = [...Array((row + 1) * 2)]
+    for (let i = 0; i < (row + 1) * 2; i++) {
+      newWords[i] = dict[i]
+    }
+    setWordList(newWords)
   }
 
   useEffect(() => {
@@ -86,8 +133,8 @@ function App() {
   return (
     <>
       <div className='container'>
-        <MainGrid changeColor={changeColor} getColor={getColor}/>
-        <WordList />
+        <MainGrid changeColor={changeColor}/>
+        <WordList wordList={wordList}/>
       </div>
 
       <Keyboard setBoxCharacter={setBoxCharacter}/>
--- wordle-solver/src/MainGrid.jsx
+++ wordle-solver/src/MainGrid.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import './App.css'
 
-const colors = ['rgb(59, 59, 59)', 'rgb(204, 175, 13)', 'rgb(34, 150, 23)']
-
-function MainGrid({ changeColor, getColor }) {
+function MainGrid({ changeColor }) {
 
   function changeColorHandler(event) {
     if (event.target.value === '') {
@@ -12,19 +10,8 @@ function MainGrid({ changeColor, getColor }) {
 
     const row = event.target.id[3]
     const col = event.target.id[4]
-    const color = getColor(row, col)
 
-    let newColor;
-    if (color === '') {
-      newColor = colors[0]
-    } else {
-      newColor = colors[(colors.indexOf(color) + 1) % colors.length]
-    }
-
-    changeColor(row, col, newColor)
-
-    event.target.style.backgroundColor = newColor
-    event.target.style.borderColor = newColor
+    changeColor(event.target, row, col)
   }
 
   return (
