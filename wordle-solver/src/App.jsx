bdf1ea2544d2fa388df56c57b76f520cc1f931f3
Oliver Persson <oliver.persson.8578@student.lu.se>
2024-12-14 18:47:40 +0100

    Added colors for boxes

--- wordle-solver/src/App.jsx
+++ wordle-solver/src/App.jsx
@@ -9,7 +9,6 @@ import WordList from './WordList'
 function App() {
   const [row, setRow] = useState(0);
   const [col, setCol] = useState(0);
-  const [count, setCount] = useState(0);
   
   function setBoxCharacter(event) {
     event.preventDefault()
@@ -63,27 +62,6 @@ function App() {
       </div>
 
       <Keyboard setBoxCharacter={setBoxCharacter}/>
-
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- wordle-solver/src/MainGrid.jsx
+++ wordle-solver/src/MainGrid.jsx
@@ -1,7 +1,57 @@
 import React from 'react';
 import './App.css';
 
+const grey = 'rgb(59, 59, 59)';
+const yellow = 'rgb(204, 175, 13)';
+const green = 'rgb(34, 150, 23)';
+
+function defaultColors() {
+  const colors = {};
+  for (let i = 0; i < 6; i++) {
+    colors[i] = {};
+    for (let j = 0; j < 5; j++) {
+      colors[i][j] = '';
+    }
+  }
+  return colors;
+}
+
+function resetRow(row) {
+  for (let j = 0; j < 5; j++) {
+    const box = document.getElementById(`col${row}${j}`);
+    box.style.backgroundColor = '';
+    box.style.borderColor = grey;
+  }
+}
+
 function MainGrid() {
+  const [colors, setColors] = React.useState(defaultColors);
+
+  function changeColor(event) {
+    if (event.target.value === '') {
+      return;
+    }
+
+    const row = event.target.id[3];
+    const col = event.target.id[4];
+    const color = colors[row][col];
+
+    let newColor;
+    if (color === '' || color === green) {
+      newColor = grey;
+    } else if (color === grey) {
+      newColor = yellow;
+    } else if (color === yellow) {
+      newColor = green;
+    }
+
+    const newColors = { ...colors, [row]: { ...colors[row], [col]: newColor } };
+    setColors(newColors);
+
+    event.target.style.backgroundColor = newColor;
+    event.target.style.borderColor = newColor;
+  }
+
   return (
     <div className="main-grid">
         {[...Array(6)].map((_, i) => (
@@ -12,7 +62,8 @@ function MainGrid() {
               type="text" 
               maxLength="1" 
               key={`col${i}${j}`}
-              id={`col${i}${j}`}>
+              id={`col${i}${j}`}
+              onClick={changeColor}>
             </input>
           ))}
         </div>
