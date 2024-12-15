b35ea02ab3150d6d29b12d6b4cd7cd881cfcfe64
Oliver Persson <oliver.persson.8578@student.lu.se>
2024-12-15 18:38:56 +0100

    Fixed pressing whole key label

--- wordle-solver/src/Keyboard.jsx
+++ wordle-solver/src/Keyboard.jsx
@@ -6,61 +6,61 @@ const keyboard = ['QWERTYUIOP', 'ASDFGHJKL', 'ZXCVBNM']
 function Keyboard({ setBoxCharacter }) {
     return (
         <div>
-        <div className='key-row'>
-        {keyboard[0].split('').map((_, i) => (
-            <li>
-                <label
-                className='key-label'
-                key={`key_${keyboard[0][i]}`} 
-                id={`key_${keyboard[0][i]}`} 
-                onClick={setBoxCharacter}>
-                {keyboard[0][i]}
-                </label>
-            </li>
-            ))}
-        </div>
+            <div className='key-row'>
+            {keyboard[0].split('').map((_, i) => (
+                <li 
+                    key={`key_${keyboard[0][i]}`} 
+                    onClick={setBoxCharacter}>
+                    <label 
+                        className='key-label'
+                        id={`key_${keyboard[0][i]}`}>
+                        {keyboard[0][i]}
+                    </label>
+                </li>
+                ))}
+            </div>
 
-        <div className='key-row'>
-            {keyboard[1].split('').map((_, i) => (
-            <li>
-                <label 
-                className='key-label'
-                key={`key_${keyboard[1][i]}`} 
-                id={`key_${keyboard[1][i]}`} 
-                onClick={setBoxCharacter}>
-                {keyboard[1][i]}
-                </label>
-            </li>
-            ))}
-        </div>
+            <div className='key-row'>
+                {keyboard[1].split('').map((_, i) => (
+                <li
+                    key={`key_${keyboard[1][i]}`} 
+                    onClick={setBoxCharacter}>
+                    <label 
+                        className='key-label' 
+                        id={`key_${keyboard[1][i]}`}>
+                        {keyboard[1][i]}
+                    </label>
+                </li>
+                ))}
+            </div>
 
-        <div className='key-row'>
-            <label 
-                className='enter-key-label'
-                key={'key_ENTER'} 
-                id={'key_ENTER'} 
-                onClick={setBoxCharacter}>
-                {'ENTER'}
-            </label>
-            {keyboard[2].split('').map((_, i) => (
-            <li>
+            <div className='key-row'>
                 <label 
-                className='key-label'
-                key={`key_${keyboard[2][i]}`} 
-                id={`key_${keyboard[2][i]}`} 
-                onClick={setBoxCharacter}>
-                {keyboard[2][i]}
+                    className='enter-key-label'
+                    key={'key_ENTER'} 
+                    id={'key_ENTER'} 
+                    onClick={setBoxCharacter}>
+                    {'ENTER'}
                 </label>
-            </li>
-            ))}
-            <label 
-            className='del-key-label'
-            key={'key_DEL'} 
-            id={'key_DEL'} 
-            onClick={setBoxCharacter}>
-            {'DEL'}
-            </label>
-        </div>
+                {keyboard[2].split('').map((_, i) => (
+                <li
+                    key={`key_${keyboard[2][i]}`} 
+                    onClick={setBoxCharacter}>
+                    <label 
+                        className='key-label'
+                        id={`key_${keyboard[2][i]}`}>
+                        {keyboard[2][i]}
+                    </label>
+                </li>
+                ))}
+                <label 
+                className='del-key-label'
+                key={'key_DEL'} 
+                id={'key_DEL'} 
+                onClick={setBoxCharacter}>
+                {'DEL'}
+                </label>
+            </div>
         </div>
     );
 }
