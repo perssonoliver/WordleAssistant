942afb064286fc47d9726d359f2cc534d995a8e4
Oliver Persson <oliver.persson.8578@student.lu.se>
2024-12-16 18:55:11 +0100

    Added hovering on keys

--- wordle-solver/src/WordList.jsx
+++ wordle-solver/src/WordList.jsx
@@ -4,7 +4,7 @@ import './App.css';
 function WordList({ wordList }) {
     return (
         <div className='word-list'>
-            <h2>Suggested words</h2>
+            <h2 className='fs-4'>Suggested words</h2>
             <ul className="list-group">
                 {wordList.map((word, i) => (
                     <li className="list-group-item" key={`word${i}`}>{word}</li>
