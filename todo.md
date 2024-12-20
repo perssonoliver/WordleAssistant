fc0996300628a710c07eea2a1d8ca151062b0ae8
Oliver Persson <oliver.persson.8578@student.lu.se>
2024-12-20 17:41:43 +0100

    Implented simple instructions

--- todo.md
+++ todo.md
@@ -3,9 +3,9 @@ TODO:
     - refactor word list filtering code
     - add functionality for pressing 'real' keyboard instead of screen keyboard
     - default item in wordlist should not be a list item 
-    - create help component with user instructions 
     - add transition when opening and closing help component
     - display feedback when pressing enter illegally
+    - change fonts
 
 BUGS: 
-    - 
+    - fix fixed width of help screen related to the screen size
--- wordle-solver/src/HelpScreen.jsx
+++ wordle-solver/src/HelpScreen.jsx
@@ -9,16 +9,19 @@ function HelpScreen({ onClose }) {
                 <button className='exit-button' onClick={onClose}>
                     <img className='exit-icon' src={exitIcon} alt='X' />
                 </button>
-                <h2 className='help-item'>Help</h2>
-                <p className='help-item'>This is the help screen content.</p>
-                <p className='help-item'>This is the help screen content.</p>
-                <p className='help-item'>This is the help screen content.</p>
-                <p className='help-item'>This is the help screen content.</p>
-                <p className='help-item'>This is the help screen content.</p>
-                <p className='help-item'>This is the help screen content.</p>
-                <p className='help-item'>This is the help screen content.</p>
-                <p className='help-item'>This is the help screen content.</p>
-                <p className='help-item'>This is the help screen content.</p>
+                <p className='help-header fs-2'>INSTRUCTIONS</p>
+                <ul className='help-list'>
+                    <li>Insert characters in the current row by pressing the keys on the keyboard.</li>
+                    <li>Modify the color of the character tile by pressing it, so that it matches 
+                        the color from Wordle.</li>
+                    <li>When all five characters are colored, press Enter to display a list of possible 
+                        valid words.</li>
+                </ul>
+                <p className='help-header fs-5'>Other tips</p>
+                <ul className='help-list'>
+                    <li>Double click a word from the word list to quick apply it.</li>
+                    <li>Reset the tiles by pressing the reset button in the top menu.</li>
+                </ul>
             </div>
         </div>
     );
